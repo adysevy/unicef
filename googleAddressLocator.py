b86d5461d4171a715050aa824dd524fd42ae18e3
Merry McCarron <mmccarron@gmail.com>
2015-04-06 16:02:24 -0400

    fixed the ascii encoding error in the google api script, changed the way that lat and lng were added so that the google api was only queried once per unique country name -- to speed up script and because i was surpassing the 24-hour query limit

--- googleAddressLocator.py
+++ googleAddressLocator.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
+# google maps geocoding API documentation: 
+# https://developers.google.com/maps/documentation/geocoding/#Geocoding
+
 import os
 import sys
 import pandas as pd
 import json
-from urllib2 import Request, urlopen, URLError
+from urllib2 import Request, urlopen, URLError, quote
 import time
 
 baseurl = "http://maps.googleapis.com/maps/api/geocode/json?address="
 
 def address_locator(locationName):
+    locationName = quote(locationName)
     request = Request(baseurl+locationName)
     try: 
         response= urlopen(request)
         locationdata = json.load(response)
     except URLError, e:
-        print "Yikes,", e
+        print e
         # print locationdata
         time.sleep(0.2)
         return {u'lat': 0.0, u'lng': 0.0}
     else:
         if locationdata['status']!='OK':
+            print locationdata['status']
             return {u'lat': 0.0, u'lng': 0.0}
         else:
+            # 
             centroid = locationdata['results'][0]['geometry']['location']
         # print response
             time.sleep(0.2)
             return centroid
+
--- mm_data_modeling.py
+++ mm_data_modeling.py
@@ -5,28 +5,31 @@ from datetime import datetime
 import pandas as pd
 import json
 import googleAddressLocator as goog
+import string
 
 punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 def remove_punctuation(s):
     s_sans_punct = ""
     for letter in s:
-        if letter not in punctuation:
+        if letter not in string.ascii_letters:
+            s_sans_punct += ' '
+        elif letter not in punctuation:
             s_sans_punct += letter
         elif letter in punctuation:
             s_sans_punct += ' '
     return s_sans_punct
 
-centroids = pd.read_csv('countrycentroidsmm.csv', usecols=['Country_name', 
-    'UNc_latitude', 'UNc_longitude'])
+# centroids = pd.read_csv('countrycentroidsmm.csv', usecols=['Country_name', 
+#     'UNc_latitude', 'UNc_longitude'])
 
 # with open('countryTranslateDict.json') as json_file:
 #     countryCrosswalk = json.load(json_file)
 
 #stripping the country names in my centroid file of punctuation, 
 #leading/trailing spaces, and converting to all lower-case.
-centroids['Country_name'] = centroids['Country_name'].apply(lambda x: remove_punctuation(x))
-centroids['Country_name'] = centroids['Country_name'].apply(lambda x: x.strip())
-centroids['Country_name'] = centroids['Country_name'].apply(lambda x: x.lower())
+# centroids['Country_name'] = centroids['Country_name'].apply(lambda x: remove_punctuation(x))
+# centroids['Country_name'] = centroids['Country_name'].apply(lambda x: x.strip())
+# centroids['Country_name'] = centroids['Country_name'].apply(lambda x: x.lower())
 
 
 dates = []
@@ -36,8 +39,8 @@ regions = []
 stories = []
 titles = []
 links = []
-lat = []
-lng = []
+# lat = []
+# lng = []
 countries_simple = []
 
 regions_codes = {'GENERAL':'General',\
@@ -54,7 +57,9 @@ regions_codes = {'GENERAL':'General',\
 folder = 'OPSCEN Brief 2014/'
 # folder = 'test/'
 
-special_files = ['UNICEF OPSCEN Brief – 29 December 2014.docx','UNICEF OPSCEN Brief – 30 December 2014.docx','UNICEF OPSCEN Brief – 31 December 2014.docx']
+special_files = ['UNICEF OPSCEN Brief – 29 December 2014.docx',
+    'UNICEF OPSCEN Brief – 30 December 2014.docx',
+    'UNICEF OPSCEN Brief – 31 December 2014.docx']
 
 flag_country = False
 flag_title = False
@@ -65,71 +70,6 @@ for file_name in os.listdir(folder):
     try:
         f = open(folder+file_name)
         document = Document(f)
-        # for p in document.paragraphs:
-            
-        #     if p.text == '' or 'Disclaimer:' in p.text or 'The Brief is produced' in p.text:
-        #             continue
-
-        #     if p.text in regions_codes:
-        #         curr_region = regions_codes[p.text]
-        #         flag_country = True
-        #         continue
-                
-        #     if 'http' in p.text:
-        #         regions.append(curr_region)
-        #         countries.append(curr_country)
-        #         titles.append(curr_title)
-        #         stories.append(curr_story)
-        #         file_names.append(file_name)
-        #         if file_name == 'UNICEF OPSCEN Brief – 29 December 2014.docx':
-        #             date = datetime(year=2014, month=12, day=29)
-        #         elif file_name == 'UNICEF OPSCEN Brief – 30 December 2014.docx': 
-        #             date = datetime(year=2014, month=12, day=30)
-        #         elif file_name == 'UNICEF OPSCEN Brief – 31 December 2014.docx':
-        #             date = datetime(year=2014, month=12, day=31)
-        #         else:
-        #             date = document.core_properties.modified
-
-        #         dates.append(date)
-        #         links.append(p.text)
-
-        #         formattedcountry = remove_punctuation(curr_country).strip().lower()
-        #         # googleGeolocation = goog.address_locator(formattedcountry)
-        #         # lat.append(googleGeolocation['lat'])
-        #         # lng.append(googleGeolocation['lng'])
-
-        #         # if 'across' in formattedcountry:
-        #         #     countries_simple.append('regional or global')
-        #         # else:
-        #         #     countries_simple.append(formattedcountry)
-        #         countries_simple.append(formattedcountry)
-
-
-        #         flag_country = True
-        #         flag_title = False
-        #         curr_story = ''
-        #         continue
-                
-        #     if flag_country:
-        #         if len(p.text.split())< 4:
-        #             curr_country = p.text
-        #             flag_country = False
-        #             flag_title = True
-        #             continue
-        #         else:
-        #             flag_country = False
-        #             flag_title = True
-
-        #     if flag_title:
-        #         curr_title = p.text
-        #         flag_title = False
-        #         flag_story = True
-        #         continue
-                
-        #     if flag_story:
-        #         curr_story = curr_story + p.text
-        #         continue
-
 
     except:
         print "Could not process file: " + file_name
@@ -164,14 +104,11 @@ for file_name in os.listdir(folder):
                 links.append(p.text)
 
                 formattedcountry = remove_punctuation(curr_country).strip().lower()
-                googleGeolocation = goog.address_locator(formattedcountry)
-                lat.append(googleGeolocation['lat'])
-                lng.append(googleGeolocation['lng'])
+                # print formattedcountry
+                # googleGeolocation = goog.address_locator(formattedcountry)
+                # lat.append(googleGeolocation['lat'])
+                # lng.append(googleGeolocation['lng'])
 
-                # if 'across' in formattedcountry:
-                #     countries_simple.append('regional or global')
-                # else:
-                #     countries_simple.append(formattedcountry)
                 countries_simple.append(formattedcountry)
 
 
@@ -217,13 +154,14 @@ Add in matching on formatted countries == centroid country names, inserting
 the appropriate lat and long into the lat and lng lists.
 """
 
-df = pd.DataFrame({'region':regions, 'country':countries, 'title': titles , 'story':stories,\
-                   'link': links,\
-                   'file_name':file_names,\
-                   'date':dates,
-                   'lat': lat,
-                   'lng': lng,
-                   'Country_name': countries_simple})
+df = pd.DataFrame({'region':regions, 'country':countries, 'title': titles, 
+                    'story':stories,\
+                    'link': links,\
+                    'file_name':file_names,\
+                    'date':dates,
+                   # 'lat': lat,
+                   # 'lng': lng,
+                    'country_simple': countries_simple})
 
 # df['country'] = df['country'].apply(lambda x: remove_punctuation(x))
 df['country'] = df['country'].apply(lambda x: x.strip())
@@ -231,13 +169,20 @@ df['country'] = df['country'].apply(lambda x: x.strip())
 
 # dfmerged = pd.merge(df, centroids, how='left', on='Country_name', left_index=False)
 
-df = pd.merge(df, centroids, how='left', on='Country_name', left_index=False)
+uniqueCountryList = df.country_simple.unique()
+lat = []
+lng = []
 
-# df.link.unique()
+for i in uniqueCountryList:
+    googleGeolocation = goog.address_locator(i)
+    lat.append(googleGeolocation['lat'])
+    lng.append(googleGeolocation['lng'])
 
-# print df.country.unique()
+centroids = pd.DataFrame(data={'country_simple': uniqueCountryList,
+    'lat': lat, 'lng': lng})
 
-# df.region.unique()
+df = pd.merge(df, centroids, how='left', on='country_simple', 
+    left_index=False)
 
 df.to_csv('OPSCENoutput.csv', encoding = 'utf-8')
 
